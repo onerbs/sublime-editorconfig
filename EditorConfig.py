6aa2a31a540a322bcaba5d2d9fbdaaca566cf339
AAAA <dev@onerbs.com>
2020-07-18 15:50:53 -0500

    Update

--- EditorConfig.py
+++ EditorConfig.py
@@ -4,181 +4,160 @@
 # | |__| (_| | | || (_) | |  | |__| (_) | | | |  _| | (_| |
 # |_____\__,_|_|\__\___/|_|   \____\___/|_| |_|_| |_|\__, |
 #                                                    |___/
-
-import sublime
-import sublime_plugin
+from sublime import View
+from sublime_plugin import EventListener, TextCommand
 
 from os import path, sep
+from re import match, split
 
 LINE_ENDINGS = {
-	'cr'   : 'cr',
-	'crlf' : 'windows',
-	'lf'   : 'unix'
+	'cr': 'cr',
+	'crlf': 'windows',
+	'lf': 'unix'
 }
 
 CHARSETS = {
-	'latin1'    : 'Western (ISO 8859-1)',
-	'utf-16be'  : 'UTF-16 BE',
-	'utf-16le'  : 'UTF-16 LE',
-	'utf-8'     : 'UTF-8',
+	'latin1': 'Western (ISO 8859-1)',
+	'utf-16be': 'UTF-16 BE',
+	'utf-16le': 'UTF-16 LE',
+	'utf-8': 'UTF-8',
 }
 
 DEBUG = True
+
+
 def debug(x: str):
 	if DEBUG:
 		print('EditorConfig: ' + x)
 
 
-class EditorConfig(sublime_plugin.EventListener):
-	def on_load(self, view):
-		dispatch('_load_', view)
+class EditorConfig(EventListener):
+	def on_load(self, view: View):
+		dispatch(view, '_load_')
 
-	def on_activated(self, view):
-		dispatch('_start_', view)
+	def on_activated(self, view: View):
+		dispatch(view, '_start_')
 
-	def on_pre_save(self, view):
-		dispatch('_save_', view)
+	def on_pre_save(self, view: View):
+		dispatch(view, '_save_')
 
 
-class RemoveFinalNewlinesCommand(sublime_plugin.TextCommand):
+def dispatch(view: View, event: str):
+	parts = path.split(view.file_name())
+	config = parse_file(find_file(parts[0]), parts[1].split('.')[-1])
+
+	if event == '_start_' or event == '_load_':
+		fix_eol(view, config.get('end_of_line'))
+		fix_charset(view, config.get('charset'))
+
+	if event == '_save_':
+		fix_indent(view, config.get('indent_style'))
+		fix_newline(view, config.get('insert_final_newline'))
+
+
+class RemoveFinalNewlinesCommand(TextCommand):
 	def run(self, edit):
-		region = self.view.find('\n*\Z', 0)
+		region = self.view.find('\n*\0', 0)
 		if region.size() > 1:
 			self.view.erase(edit, region)
 			debug('Remove final newlines')
 
-class NormalFinalNewlinesCommand(sublime_plugin.TextCommand):
+
+class NormalizeFinalNewlinesCommand(TextCommand):
 	def run(self, edit):
-		region = self.view.find('\n*\Z', 0)
+		region = self.view.find('\n*\0', 0)
 		if region.size() > 1:
 			self.view.replace(edit, region, '\n')
 			debug('Normalize final newlines')
 
-#------------------------------------------------------------------------------#
 
-def dispatch(event, view):
-	file_name = view.file_name()
-	config = _mkconfig(file_name)
-
-	if not config:
-		return
-
-	if event == '_start_' or event == '_load_':
-		charset = config.get('charset')
-		encoding = view.encoding()
-		if (
-			encoding != 'Undefined' and
-			charset in CHARSETS and
-			CHARSETS[charset] != encoding
-		):
-			new_encoding = CHARSETS[charset]
-			view.set_encoding(new_encoding)
-			debug('Updated charset from '+encoding+' to '+new_encoding)
-
-		eol = config.get('end_of_line')
-		if eol in LINE_ENDINGS and LINE_ENDINGS[eol] != view.line_endings().lower():
-			view.set_line_endings(LINE_ENDINGS[eol])
-			debug('Updated EOL')
-
-		indent_style = config.get('indent_style')
-		spaces = view.settings().get('translate_tabs_to_spaces')
-		if indent_style == 'space' and spaces == False:
-			view.run_command('expand_tabs', {'set_translate_tabs': True})
-			debug('Update Using spaces')
-		elif indent_style == 'tab' and spaces == True:
-			view.run_command('unexpand_tabs', {'set_translate_tabs': True})
-			debug('Update Using tabs')
-
-	if event == '_save_':
-		insert_final_newline = config.get('insert_final_newline')
-		ensure_newline_at_eof_on_save = view.settings().get('ensure_newline_at_eof_on_save')
-		if insert_final_newline == 'true' or ensure_newline_at_eof_on_save:
-			view.run_command('normal_final_newlines')
-		else:
-			view.run_command('remove_final_newlines')
+# ---------------------------------------------------------------------------- #
 
 
-def _mkconfig(file_name):
-	if None == file_name:
-		return False
-	folder, file = path.split(file_name)
-	config = parse_file(folder, file)
+def fix_charset(view: View, charset: str):
+	encoding = view.encoding()
+	if (
+		encoding != 'Undefined' and
+		charset in CHARSETS and
+		CHARSETS[charset] != encoding
+	):
+		new_encoding = CHARSETS[charset]
+		view.set_encoding(new_encoding)
+		debug('Updated charset from ' + encoding + ' to ' + new_encoding)
 
-	# Set indent_size to "tab" if indent_size is unspecified and
-	# indent_style is set to "tab".
-	if config.get("indent_style") == "tab" and not "indent_size" in config:
-		config["indent_size"] = "tab"
 
-	# Set tab_width to indent_size if indent_size is specified and
-	# tab_width is unspecified
-	if ("indent_size" in config and "tab_width" not in config and
-			config["indent_size"] != "tab"):
-		config["tab_width"] = config["indent_size"]
+def fix_eol(view: View, eol: str):
+	if eol in LINE_ENDINGS and LINE_ENDINGS[eol] != view.line_endings().lower():
+		view.set_line_endings(LINE_ENDINGS[eol])
+		debug('Updated EOL')
 
-	# Set indent_size to tab_width if indent_size is "tab"
-	if ("indent_size" in config and "tab_width" in config and
-			config["indent_size"] == "tab"):
-		config["indent_size"] = config["tab_width"]
 
-	return config
+def fix_indent(view: View, indent_style: str):
+	spaces = view.settings().get('translate_tabs_to_spaces')
+	if indent_style == 'space' and spaces is False:
+		view.run_command('expand_tabs', {'set_translate_tabs': True})
+		debug('Update Using spaces')
+	elif indent_style == 'tab' and spaces is True:
+		view.run_command('unexpand_tabs', {'set_translate_tabs': True})
+		debug('Update Using tabs')
 
-#------------------------------------------------------------------------------#
 
-from re import match, split
+def fix_newline(view: View, insert_final_newline: str):
+	ensure_newline_at_eof_on_save = view.settings().get('ensure_newline_at_eof_on_save')
+	if insert_final_newline == 'true' or ensure_newline_at_eof_on_save:
+		view.run_command('normalize_final_newlines')
+	else:
+		view.run_command('remove_final_newlines')
 
-def parse_file(file_root, file_name):
 
-	ec_file = path.join(file_root, '.editorconfig')
+# ---------------------------------------------------------------------------- #
+
+
+def find_file(root: str) -> str:
+	file = path.join(root, '.editorconfig')
+	if not path.exists(file):
+		parent = path.split(root)[0]
+		return '' if parent == root else find_file(parent)
+	return file
+
+
+def parse_file(file: str, extension: str) -> dict:
 	options = {}
-
-	if not path.exists(ec_file):
-		prent_folder = path.dirname(file_root)
-		return options if prent_folder == file_root else parse_file(prent_folder, file_name)
-
-	aplicable = False
-	lines = list(map(lambda s: s.lower().strip(), open(ec_file).readlines()))
-
-	for line in lines:
-		# comment or blank line?
-		if line == '' or line[0] in '#;':
-			continue
-
-		# is it a section header?
-		if line[0] == '[':
-			pattern = line[1:-1]
-			aplicable = fnmatch(file_name, pattern)
-			continue
-
-		if not aplicable:
-			continue
-
-		if '=' in line:
-			optname, optval = list(map(lambda s: s.strip(), line.split('=', 2)))
-			if match(' [#;]', optval):
-				optval = split(' [#;]', optval)[0]
-			# allow empty values
-			if optval == '""':
-				optval = ''
-
-			options[optname] = optval
-
+	if file:
+		applicable = False
+		for line in get_lines(file):
+			if line[0] == '[':
+				applicable = matches(line[1:-1], extension)
+				continue
+			if not applicable:
+				continue
+			if '=' in line:
+				option, value = split(' *= *', line, 1)
+				options[option] = value
 	return options
 
 
-def fnmatch(name, pattern):
+def get_lines(file: str) -> list:
+	lines = []
+	for line in open(file).readlines():
+		if line.strip():
+			lines.append(split('[#;]', line)[0].strip().lower())
+	return lines
+
+
+def matches(pattern: str, extension: str) -> bool:
 	pattern = clean_pattern(pattern)
-	name = name.split('.')[-1]
-	for p in pattern.split(','):
-		if p == '*' or name == p:
+	for part in pattern.split(','):
+		if part is '*' or part is extension:
 			return True
 	return False
 
 
-def clean_pattern(p):
-	if p == '*':
-		return p
-	r = ''
-	for c in p:
-		if match('[a-z,-]', c):
-			r = r + c
-	return r
+def clean_pattern(pattern: str) -> str:
+	if pattern is '*':
+		return pattern
+	clean = ''
+	for char in pattern:
+		if match('[a-z,-]', char):
+			clean += char
+	return clean
